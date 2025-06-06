3c50aefa022684b3c5b28b8a51630cffa7ed2d20
Chami204 <chamidimanthi@gmail.com>
2025-06-06 10:50:15 +0530

    Update packing_costing_app.py

--- packing_costing_app.py
+++ packing_costing_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
+# Set up page configuration
+st.set_page_config(page_title="Packing Costing Document", page_icon="📄")
+
 # Title
-st.title("Packing Costing App")
+st.title("📦 Packing Costing App")
 
 # Step 01 - Inputs
 st.header("Step 01")
